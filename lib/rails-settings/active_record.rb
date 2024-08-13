176f1958cbf51f51a090bbec0a1bf8b6d8f6f031
David Cox Jr <davemc08@gmail.com>
2024-08-13 11:05:33 -0400

    make scopes rails 4.2 compatible

--- lib/rails-settings/active_record.rb
+++ lib/rails-settings/active_record.rb
@@ -15,24 +15,24 @@ ActiveRecord::Base.class_eval do
 
       after_destroy { |user| user.settings.target_scoped.delete_all }
 
-      scope_method = ActiveRecord::VERSION::MAJOR < 3 ? :named_scope : :scope
-      
-      send scope_method, :with_settings, :joins => "JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
-                                                                      settings.target_type = '#{self.base_class.name}')",
-                                         :select => "DISTINCT #{self.table_name}.*" 
+      send :scope, :with_settings, -> do
+        joins("JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND settings.target_type = '#{self.base_class.name}')")
+          .select("DISTINCT #{self.table_name}.*")
+      end
 
-      send scope_method, :with_settings_for, lambda { |var| { :joins => "JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
-                                                                              settings.target_type = '#{self.base_class.name}') AND
-                                                                              settings.var = '#{var}'" } }
-                                                               
-      send scope_method, :without_settings, :joins => "LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
-                                                                 settings.target_type = '#{self.base_class.name}')",
-                                            :conditions => 'settings.id IS NULL'
-                                     
-      send scope_method, :without_settings_for, lambda { |var| { :joins => "LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND
-                                                                                      settings.target_type = '#{self.base_class.name}') AND
-                                                                                      settings.var = '#{var}'",
-                                                                 :conditions => 'settings.id IS NULL' } }
+      send :scope, :with_settings_for, ->(var) do
+        joins("JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND settings.target_type = '#{self.base_class.name}') AND settings.var = '#{var}'")
+      end
+
+      send :scope, :without_settings, -> do
+        joins("LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND settings.target_type = '#{self.base_class.name}')")
+          .where("settings.id IS NULL")
+      end
+
+      send :scope, :without_settings_for, ->(var) do
+        joins("LEFT JOIN settings ON (settings.target_id = #{self.table_name}.#{self.primary_key} AND settings.target_type = '#{self.base_class.name}') AND settings.var = '#{var}'")
+          .where("settings.id IS NULL")
+      end
     end
   end
 end
